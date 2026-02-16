d17efeb0f26ec68ca6ce9386fa2ba8eb777b4e35
Oscar Muñoz <omunoz@canella.com.gt>
2026-02-16 17:28:09 -0600

    Añadido el datatable de busqueda por factura IVEMRI.cs

--- os_STOD_DAL/IVEMRI.cs
+++ os_STOD_DAL/IVEMRI.cs
@@ -1,12 +1,35 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace STOD_DAL
 {
-    class IVEMRI
+    public class IVEMRI
     {
+
+        #region datatable IVEMBRI BUSQUEDA POR FACTURA
+        public DataTable IVEMRI_BUSQUEDA(List<CL_Diccionario> ListParamsIn, out CL_TipoMensaje Obj_Mensaje)
+        {
+            Obj_Mensaje = new CL_TipoMensaje();
+            DataTable dt = new DataTable("Datos");
+            try
+            {
+                DAL DAC = new DAL();
+                string Procedimiento = "IVEMRI_ConsultaMatrizRiesgoFactura";
+                string conectionStringNombre = "ServidorCanellaService"; // Asegúrate que este nombre esté en tu Web.config
+                dt = DAC.EjecutarQueryPorPAConRetorno(Procedimiento, conectionStringNombre, ListParamsIn, out Obj_Mensaje);
+            }
+            catch (Exception ex)
+            {
+                Obj_Mensaje.MensajeTipo = 0;
+                Obj_Mensaje.MensajeDescripcion = ex.Message;
+            }
+            return dt;
+        }
+        #endregion
     }
 }
